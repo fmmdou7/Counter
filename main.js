6de47ed5859005608c27f057cf56b4b847a75b95
Fatma <70917212+fmmdou7@users.noreply.github.com>
2024-04-18 00:26:01 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+let num=0;
+
+function Decrese(){
+
+    document.getElementById("numb").innerHTML = --num;
+
+
+}
+
+function Reset(){
+
+    document.getElementById("numb").innerHTML = 0;
+    num=0;
+
+}
+
+function Increase(){
+    document.getElementById("numb").innerHTML = ++num;
+
+}
+
